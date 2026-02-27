f57823a63a5a430fd561f98e36c74a31ff9ca5d3
tijanastojanovska <stojanovska_tijana@outlook.com>
2026-02-27 15:13:00 +0100

    unit tests

---
+++ BookSystem.Test/BookingServiceTestcs.cs
@@ -0,0 +1,169 @@
+﻿using BookingSystem.Business.Implementation;
+using BookingSystem.DataAccess.Interfaces.Repositories;
+using BookingSystem.Domain.DomainModels;
+using BookingSystem.Domain.Enums;
+using BookingSystem.DTOs.Booking;
+using Moq;
+
+namespace BookingSystem.Test
+{
+	[TestFixture]
+	public class BookingServiceTest
+	{
+		private Mock<ISearchRepository> _searchRepoMock;
+		private Mock<IBookingRepository> _bookingRepoMock;
+
+		private BookingService _bookingService;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_searchRepoMock = new Mock<ISearchRepository>();
+			_bookingRepoMock = new Mock<IBookingRepository>();
+
+			_bookingService = new BookingService(
+				_searchRepoMock.Object,
+				_bookingRepoMock.Object);
+		}
+
+		[Test]
+		public void Book_WhenRequestIsNull_ThrowsArgumentException()
+		{
+			Assert.That(() => _bookingService.Book(null),
+				Throws.TypeOf<ArgumentException>());
+		}
+
+		[Test]
+		public void Book_WhenOptionCodeIsEmpty_ThrowsArgumentException()
+		{
+			BookRequest request = new BookRequest
+			{
+				OptionCode = " "
+			};
+
+			Assert.That(() => _bookingService.Book(request),
+				Throws.TypeOf<ArgumentException>());
+		}
+
+		[Test]
+		public void Book_WhenOptionDoesNotExist_ThrowsKeyNotFound()
+		{
+			BookRequest request = new BookRequest
+			{
+				OptionCode = "OPT1"
+			};
+
+			_searchRepoMock
+				.Setup(r => r.GetOption("OPT1"))
+				.Returns((Option)null);
+
+			Assert.That(() => _bookingService.Book(request),
+				Throws.TypeOf<KeyNotFoundException>());
+		}
+
+		[Test]
+		public void Book_WhenValidRequest_SavesBookingAndReturnsResponse()
+		{
+			BookRequest request = new BookRequest
+			{
+				OptionCode = "OPT1"
+			};
+
+			Option option = new Option { OptionCode = "OPT1" };
+
+			_searchRepoMock
+				.Setup(r => r.GetOption("OPT1"))
+				.Returns(option);
+
+			_searchRepoMock
+				.Setup(r => r.GetSearchType("OPT1"))
+				.Returns(SearchTypeEnum.HotelOnly);
+
+			BookResponse response = _bookingService.Book(request);
+
+			_bookingRepoMock.Verify(r => r.Save(It.IsAny<Booking>()), Times.Once);
+
+			Assert.That(response, Is.Not.Null);
+			Assert.That(response.BookingCode, Is.Not.Null.And.Not.Empty);
+		}
+
+
+		[Test]
+		public void CheckStatus_WhenBookingCodeIsEmpty_ThrowsArgumentException()
+		{
+			Assert.That(() => _bookingService.CheckStatus(" "),
+				Throws.TypeOf<ArgumentException>());
+		}
+
+		[Test]
+		public void CheckStatus_WhenBookingNotFound_ThrowsKeyNotFound()
+		{
+			_bookingRepoMock
+				.Setup(r => r.Get("ABC123"))
+				.Returns((Booking)null);
+
+			Assert.That(() => _bookingService.CheckStatus("ABC123"),
+				Throws.TypeOf<KeyNotFoundException>());
+		}
+
+		[Test]
+		public void CheckStatus_WhenBookingNotReady_ReturnsPending()
+		{
+			Booking booking = new Booking
+			{
+				BookingCode = "ABC123",
+				BookingTime = DateTime.UtcNow,
+				SleepTime = 60,
+				SearchType = SearchTypeEnum.HotelOnly
+			};
+
+			_bookingRepoMock
+				.Setup(r => r.Get("ABC123"))
+				.Returns(booking);
+
+			BookingStatusResponse result = _bookingService.CheckStatus("ABC123");
+
+			Assert.That(result.Status, Is.EqualTo(BookingStatusEnum.Pending));
+		}
+
+		[Test]
+		public void CheckStatus_WhenReadyAndLastMinute_ReturnsFailed()
+		{
+			Booking booking = new Booking
+			{
+				BookingCode = "ABC123",
+				BookingTime = DateTime.UtcNow.AddMinutes(-5),
+				SleepTime = 1,
+				SearchType = SearchTypeEnum.LastMinuteHotels
+			};
+
+			_bookingRepoMock
+				.Setup(r => r.Get("ABC123"))
+				.Returns(booking);
+
+			BookingStatusResponse result = _bookingService.CheckStatus("ABC123");
+
+			Assert.That(result.Status, Is.EqualTo(BookingStatusEnum.Failed));
+		}
+
+		[Test]
+		public void CheckStatus_WhenReadyAndNotLastMinute_ReturnsSuccess()
+		{
+			Booking booking = new Booking
+			{
+				BookingCode = "ABC123",
+				BookingTime = DateTime.UtcNow.AddMinutes(-5),
+				SleepTime = 1,
+				SearchType = SearchTypeEnum.HotelOnly
+			};
+
+			_bookingRepoMock
+				.Setup(r => r.Get("ABC123"))
+				.Returns(booking);
+
+			BookingStatusResponse result = _bookingService.CheckStatus("ABC123");
+
+			Assert.That(result.Status, Is.EqualTo(BookingStatusEnum.Success));
+		}
+	}
+}
---
+++ BookSystem.Test/SearchServiceTest.cs
@@ -0,0 +1,122 @@
+﻿using BookingSystem.Business.Implementation;
+using BookingSystem.Business.Interfaces.Factories;
+using BookingSystem.Business.Interfaces.Strategies;
+using BookingSystem.DataAccess.Interfaces.Repositories;
+using BookingSystem.Domain.DomainModels;
+using BookingSystem.Domain.Enums;
+using BookingSystem.DTOs.Search;
+using Moq;
+
+namespace BookSystem.Test;
+
+[TestFixture]
+public class SearchServiceTest
+{
+	private Mock<ISearchStrategyFactory> _factoryMock;
+	private Mock<ISearchRepository> _repoMock;
+
+	private SearchService _searchService;
+
+	[SetUp]
+	public void SetUp()
+	{
+		_factoryMock = new Mock<ISearchStrategyFactory>();
+		_repoMock = new Mock<ISearchRepository>();
+
+		_searchService = new SearchService(_factoryMock.Object, _repoMock.Object);
+	}
+
+	[Test]
+	public void SearchAsync_WhenRequestIsNull_ThrowsArgumentException()
+	{
+		SearchRequest request = null;
+
+		Assert.That(
+			async () => await _searchService.SearchAsync(request),
+			Throws.TypeOf<ArgumentException>());
+	}
+
+	[Test]
+	public void SearchAsync_WhenDestinationIsEmpty_ThrowsArgumentException()
+	{
+		SearchRequest request = new SearchRequest
+		{
+			Destination = " ",
+			FromDate = DateTime.UtcNow.AddDays(10),
+			ToDate = DateTime.UtcNow.AddDays(12)
+		};
+
+		Assert.That(
+			async () => await _searchService.SearchAsync(request),
+			Throws.TypeOf<ArgumentException>());
+	}
+
+	[Test]
+	public void SearchAsync_WhenFromDateIsAfterToDate_ThrowsArgumentException()
+	{
+		SearchRequest request = new SearchRequest
+		{
+			Destination = "SKP",
+			FromDate = DateTime.UtcNow.AddDays(10),
+			ToDate = DateTime.UtcNow.AddDays(5)
+		};
+
+		Assert.That(
+			async () => await _searchService.SearchAsync(request),
+			Throws.TypeOf<ArgumentException>());
+	}
+
+	[Test]
+	public async Task SearchAsync_WhenValidRequest_CallsStrategyAndSavesAndReturnsResponse()
+	{
+		SearchRequest request = new SearchRequest
+		{
+			Destination = "SKP",
+			DepartureAirport = "OSL",
+			FromDate = DateTime.UtcNow.AddDays(60),
+			ToDate = DateTime.UtcNow.AddDays(65)
+		};
+
+		var strategyMock = new Mock<ISearchStrategy>();
+		strategyMock.SetupGet(s => s.Type)
+			.Returns(SearchTypeEnum.HotelAndFlight);
+
+		List<Option> options = new List<Option>
+		{
+			new Option
+			{
+				OptionCode = "OPT001",
+				HotelCode = "8626",
+				FlightCode = "461",
+				ArrivalAirport = "SKP",
+				Price = 120
+			},
+			new Option
+			{
+				OptionCode = "OPT002",
+				HotelCode = "8627",
+				FlightCode = "462",
+				ArrivalAirport = "SKP",
+				Price = 130
+			}
+		};
+
+		strategyMock
+			.Setup(s => s.SearchAsync(It.IsAny<SearchCriteria>()))
+			.ReturnsAsync(options);
+
+		_factoryMock
+			.Setup(f => f.GetStrategy(It.IsAny<SearchCriteria>()))
+			.Returns(strategyMock.Object);
+
+		SearchResponse result = await _searchService.SearchAsync(request);
+
+		_repoMock.Verify(
+			r => r.SaveInMemory(SearchTypeEnum.HotelAndFlight, options),
+			Times.Once);
+
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Options, Is.Not.Null);
+		Assert.That(result.Options.Count, Is.EqualTo(2));
+	}
+}
